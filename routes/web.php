1c4e4a7cbe4976a6a33a42a03542e51ee4fc1e10
tganiullin <79270440968@yandex.ru>
2026-03-11 13:52:52 +0300

    todo

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// TODO: Вынести роуты в api.php и фронт крутить как отдельный сервис
+// TODO: Вынести роуты в api.php и фронт крутить как отдельный сервис, использовать единый стандарт ответов API JSON:API specification или JSEND
+
 Route::prefix('public/positions')->name('public-positions.')->group(function (): void {
     Route::get('{token}', [PublicPositionInterviewController::class, 'show'])->name('show');
     Route::post('{token}/start', [PublicPositionInterviewController::class, 'start'])
