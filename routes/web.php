754d80e5bc3b443eae779f50f8c98f50bd685eda
tganiullin <79270440968@yandex.ru>
2026-03-21 14:01:47 +0300

    redirect aya.ru

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\PublicInterviewRunController;
 use App\Http\Controllers\PublicPositionInterviewController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'https://aya.ru');
 // TODO: Вынести роуты в api.php и фронт крутить как отдельный сервис, использовать единый стандарт ответов API JSON:API specification или JSEND
 
 Route::prefix('public/positions')->name('public-positions.')->group(function (): void {
