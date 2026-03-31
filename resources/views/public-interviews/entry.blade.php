76f747953771882ff5a4110f91b137ed8dabc32e
tganiullin <79270440968@yandex.ru>
2026-03-31 12:03:06 +0300

    zoom

--- resources/views/public-interviews/entry.blade.php
+++ resources/views/public-interviews/entry.blade.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $position->title }} — интервью</title>
     @vite(['resources/css/app.css', 'resources/js/entry-position-vue.js'])
